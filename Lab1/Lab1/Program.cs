21689499092fff06fdd11e7f307023d3b00f76fd
lukebacopoulos <105564913+lukebacopoulos@users.noreply.github.com>
2023-04-12 18:09:14 -0400

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+namespace FibonacciNumbers
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        public static void Main(string[] args)
+        {
+            int printcount = 0;
+            int fibcount = 2;
+            int n;
+
+            Console.WriteLine("Luke Bacopoulos");
+
+            while (printcount < 10)
+            {
+                n = Fibonacci(fibcount);
+                if (n >= 100)
+                {
+                    Console.WriteLine("{0} ", n);
+                    printcount++;
+                }
+                fibcount++;
+            }
+
+        }
+
+
+
+    }
+}
+
